60905a150df5e135f6a5a5c024b65d7e25afccb0
Mariana Oliveira <olvr.mariana@gmail.com>
2019-06-13 21:56:36 -0300

    Colocamos o formulário, conectamos o front com o back (banco de dados)

--- script.js
+++ script.js
@@ -1,28 +1,72 @@
 const container = document.querySelector('#items-cardapio')
-fetch(`http://localhost:3000/comidas`)
-    .then((response) => {
-        return response.json();
+fetch('http://localhost:5000/comidas')
+  .then((response) =>{
+    return response.json();
+  })
+  .then((comidas) =>{
+    comidas.forEach(prato => {
+      const mediaItem = document.createElement('div');
+      mediaItem.setAttribute('class', 'media mb-4');
+      mediaItem.innerHTML = `
+      <img src="${prato.imagem}" alt="${prato.nome}" class="mr-3 img-thumbnail" width="200px">
+
+      <div class="media-body>
+
+          <h5 class="mt-0"><strong>${prato.nome}</strong></h5>
+          ${prato.descricao}
+        </div>`
+
+        container.appendChild(mediaItem);
+        const buttonDelete = document.createElement("button")
+        buttonDelete.textContent = "Remover"
+        buttonDelete.setAttribute("class", "btn btn-info")
+        buttonDelete.setAttribute("data-id", prato._id)
+        mediaItem.appendChild(buttonDelete)
+
+        buttonDelete.addEventListener("click", () =>{
+          fetch(
+            `http://localhost:5000/comidas/${prato._id}`,
+            {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            }
+          ).then((response) => {
+            console.log(response)
+            if(response.status === 204) {
+              window.location.reload()
+            } else {
+              window.alert("Deu erro ao deletar, sorry")
+            }
+          })
+        })
     })
-    .then((data) => {
+  })
+  .catch((erro)=>{
+    console.log(erro)
+  })
 
-        data.pratosFavoritos.forEach(prato => {
-            console.log(prato)
+const botao = document.querySelector('#criar_comida_button')
+botao.addEventListener("click", criarComida)
 
-            const mediaItem = document.createElement('div');
-            mediaItem.setAttribute('class', 'media mb-4');
-            mediaItem.innerHTML = `
-            <img src="${prato.imagem}" alt="${prato.nome}" class="mr-3 img-thumbnail" width="200px">
+function criarComida () {
+  const nome = document.querySelector("#nome_input").value
+  const descricao = document.querySelector("#descricao_input").value
+  const imagem = document.querySelector("#imagem_input").value
 
-            <div class="media-body>
+  const comida = {
+    nome, descricao, imagem
+  }
 
-                <h5 class="mt-0"><strong>${prato.nome}</strong></h5>
-                ${prato.descricao}
-              </div>`
-            container.appendChild(mediaItem);
-        }
-        )
+  fetch(
+    'http://localhost:5000/comidas',
+    {
+      method: 'POST',
+      body: JSON.stringify(comida),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-    )
-    .catch((erro) => {
-        console.log(erro)
-    })
+  ).then(response => console.log("criou!"))
+}
